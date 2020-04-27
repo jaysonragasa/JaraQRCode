b554bee2e3d81403fd739379de4918ec45565e35
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-04-28 04:36:13 +0800

    Update Tue 04/28/2020 /  4:36:13.09

--- JaraQRCode.WPF/MainWindow.xaml.cs
+++ JaraQRCode.WPF/MainWindow.xaml.cs
@@ -24,14 +24,25 @@ namespace JaraQRCode.WPF
 
         private void TxText_TextChanged(object sender, TextChangedEventArgs e)
         {
-            var bmp = qr.Generate(txText.Text);
+            var byt = qr.Generate(txText.Text);
 
-            if (bmp != null)
+            if (byt != null)
             {
-                imgQRCode.Source = Convert(bmp);
+                imgQRCode.Source = Convert(byt);
                 txQRstring.Text = qr.GenerateQRCodeString(txText.Text, Encoding.UTF8);
             }
+        }
 
+        public BitmapImage Convert(byte[] src)
+        {
+            MemoryStream ms = new MemoryStream(src);
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
         }
 
         //public BitmapImage Convert(Bitmap src)
@@ -45,17 +56,5 @@ namespace JaraQRCode.WPF
         //    image.EndInit();
         //    return image;
         //}
-
-        public BitmapImage Convert(byte[] src)
-        {
-            MemoryStream ms = new MemoryStream(src);
-
-            BitmapImage image = new BitmapImage();
-            image.BeginInit();
-            ms.Seek(0, SeekOrigin.Begin);
-            image.StreamSource = ms;
-            image.EndInit();
-            return image;
-        }
     }
 }
